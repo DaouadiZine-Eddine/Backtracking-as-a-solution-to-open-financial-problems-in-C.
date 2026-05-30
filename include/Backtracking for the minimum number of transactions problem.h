9d8a0f13b5c09a1ffbd388eaaad2347d948c9989
DAOUADI Zine-Eddine Chahine <daouadi.zineeddine@gmail.com>
2026-05-30 20:20:10 +0200

    Update Backtracking for the minimum number of transactions problem.h

--- include/Backtracking for the minimum number of transactions problem.h
+++ include/Backtracking for the minimum number of transactions problem.h
@@ -1,9 +1,7 @@
 #ifndef MIN_TRANSACTIONS_H
 #define MIN_TRANSACTIONS_H
 
-
 #include <stdbool.h>
-
 
 typedef struct {
     double id;
@@ -16,11 +14,14 @@ typedef struct List_Person {
 } person_list;
 
 person_list* add_person(person_list* l, person p);
-
 person_list* remove_person(person_list* l, person p);
-
 bool search_person(person_list* l, double id);
 
-int min_transactions(double balances[], int n);
+void set_balances(double new_balances[], int n);
+bool is_possible(int i, int j);
+double perform_transaction(int i, int j);
+void cancel_transaction(int i, int j, double amount);
+bool is_final_state(void);
+void backtrack(int transaction_count, int* best_found);
 
 #endif
